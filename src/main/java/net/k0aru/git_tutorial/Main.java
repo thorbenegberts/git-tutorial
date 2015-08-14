27253401d5b6b2f873e11dd1c2ce693adecc45f3
Thorben Egberts <tegberts@gmail.com>
2015-08-14 13:19:44 +0200

    Revert "My Change"

--- src/main/java/net/k0aru/git_tutorial/Main.java
+++ src/main/java/net/k0aru/git_tutorial/Main.java
@@ -12,13 +12,13 @@ public class Main {
 
   public static void main(String[] args) {
 
-    heyJoe();
+    hello();
 
   }
 
-  private static void helloJoe() {
+  private static void hello() {
 
-    System.out.println("Hey Joe, where you goin' with that gun in your hand?");
+    System.out.println("Hello World!");
 
   }
 
